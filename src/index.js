c3c10dac6ea43ff47416705f4a4f688e691c2f65
Leonardo Situmorang <leonardo@situmorang.net>
2018-02-13 14:14:00 +0700

    Adjustments for React v16

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
  * react-native-swiper
  * @author leecade<leecade@163.com>
  */
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
   Text,
   View,
@@ -11,7 +12,8 @@ import {
   TouchableOpacity,
   ViewPagerAndroid,
   Platform,
-  ActivityIndicator
+  ActivityIndicator,
+  ViewPropTypes
 } from 'react-native'
 
 const { width, height } = Dimensions.get('window')
@@ -100,7 +102,7 @@ export default class extends Component {
   static propTypes = {
     horizontal: PropTypes.bool,
     children: PropTypes.node.isRequired,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     pagingEnabled: PropTypes.bool,
     showsHorizontalScrollIndicator: PropTypes.bool,
     showsVerticalScrollIndicator: PropTypes.bool,
